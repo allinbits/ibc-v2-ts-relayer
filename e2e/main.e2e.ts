1632af0017b8f269f7fe2bd8c6f741e545416cd8
Alex M. - Clockwork <alex@clockwork.gr>
2025-07-14 09:27:22 +0300

    chore: fix workflow

--- e2e/main.e2e.ts
+++ e2e/main.e2e.ts
@@ -1,1 +1,27 @@
-console.log("Will run e2e");
+import {
+  test,
+} from "vitest";
+
+import {
+  Relayer,
+} from "../src/relayer";
+import {
+  ChainType,
+} from "../src/types";
+import {
+  log,
+} from "../src/utils/logging";
+
+const relayer = new Relayer(log);
+
+const init = async () => {
+  await relayer.addNewdRelayPath("mars", "http://localhost:26657", "venus", "http://localhost:26658", ChainType.Cosmos, ChainType.Cosmos, 1);
+  await relayer.addNewdRelayPath("mars", "http://localhost:26657", "venus", "http://localhost:26658", ChainType.Cosmos, ChainType.Cosmos, 2);
+  await relayer.init();
+  await relayer.start();
+};
+
+test("Start relayer and. run E2E tests", async () => {
+  await init();
+  console.log("Relayer initialized and started successfully.");
+}, 60000);
