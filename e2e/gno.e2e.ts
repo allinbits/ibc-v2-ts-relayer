1478686cc21fb00a8640c69531bc38e16fe092b9
“Clockwork” <alex@clockwork.gr>
2026-03-02 12:37:52 +0200

    feat: counterparty gno tests

--- e2e/gno.e2e.ts
+++ e2e/gno.e2e.ts
@@ -1,4 +1,14 @@
 import {
+  QueryClient, setupAuthExtension, setupBankExtension, setupIbcExtension, setupStakingExtension,
+} from "@cosmjs/stargate";
+import {
+  connectComet,
+} from "@cosmjs/tendermint-rpc";
+import {
+  connectTm2,
+} from "@gnolang/tm2-rpc";
+import {
+  expect,
   test,
 } from "vitest";
 
@@ -11,6 +21,9 @@ import {
 import {
   log,
 } from "../src/utils/logging";
+import {
+  setupIbcV2Extension,
+} from "../src/v2queries/ibc.ts";
 
 const relayer = new Relayer(log);
 
@@ -30,5 +43,22 @@ const init = async () => {
 test("Start relayer and. run E2E tests", async () => {
   await init();
   // Wait for the relayer to initialize and start
+  const tmClient = await connectComet("http://localhost:56657");
+  const gnoClient = await connectTm2("http://localhost:46657");
+  const clientB = await gnoClient.abciQuery({
+    path: "vm/qrender",
+    data: Buffer.from("gno.land/r/aib/ibc/core:clients/07-tendermint-1", "utf-8"),
+  });
+  const clientState = JSON.parse(Buffer.from(clientB.responseBase.data).toString("utf-8"));
+  console.log("Client State:", clientState);
+  
+  const queryA = QueryClient.withExtensions(
+    tmClient, setupAuthExtension, setupBankExtension, setupIbcExtension, setupStakingExtension, setupIbcV2Extension,
+  );
+
+  const counterA = await queryA.ibc.clientV2.counterparty("07-tendermint-1");
+  expect(counterA).toBeDefined();
+  expect(counterA.counterpartyInfo?.clientId).toBe("10-gno-0");
+
   console.log("Relayer initialized and started successfully.");
 }, 120000);
